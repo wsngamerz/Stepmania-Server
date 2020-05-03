662878e2557fe6aec93fd9d280adefd09f097531
William Neild <wneild5@gmail.com>
2020-05-03 12:57:31 +0100

    Update build script

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
+Rem Build StepmaniaServer
+
+Rem This should package in one file but a bug with this is causing errors with including some external
+Rem Packages so unable to package in 1 file.
+Rem dotnet publish -r win-x64 -c Release /p:PublishTrimmed=true /p:PublishSingleFile=true
+
+dotnet publish -r win-x64 -c Release /p:PublishTrimmed=true
